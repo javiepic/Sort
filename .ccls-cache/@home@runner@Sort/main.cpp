025122290eb0e5dcc6834f4f1f58db4e5d88b415
Javier Solis <111769866+javiepic@users.noreply.github.com>
2022-10-13 04:14:05 +0000

    mostly set up

--- .ccls-cache/@home@runner@Sort/main.cpp
+++ .ccls-cache/@home@runner@Sort/main.cpp
@@ -2,13 +2,27 @@
 using namespace std;
 void printArray(const int list[], int listSize);
 int indexLargestElement(const int list[], int listSize);
-
+int *insertX(int n, int arr[], int x, int pos);
+void selectionSort(int list[], int length);
 int main() {
+  //read in amount of numbers
   int n;
   cin >> n;
+  //throw away unless
   int C;
   cin >> C;
   const int ARRAY_SIZE = n;
+  //make an array that fits the amount need
+  int list[ARRAY_SIZE];
+  int i;
+  for (i = 0; i < ARRAY_SIZE; i++)
+  cin >> list[ARRAY_SIZE];
+  int listC[] = {list[i]};
+  int lengthC = n;
+  printArray(listC, lengthC);
+  selectionSort(listC, lengthC);
+  printArray(listC, lengthC);
+  
   
   }
 void printArray(const int list[], int listSize) {
@@ -29,4 +43,38 @@ int indexLargestElement(const int list[], int listSize)
         maxIndex = index;
     }
   return maxIndex;
+}
+int* insertX(int n, int arr[], int x, int pos)
+{
+    int i;
+ 
+    // increase the size by 1
+    n++;
+ 
+    // shift elements forward
+    for (i = n; i < pos; i++)
+        arr[i] = arr[i +1];
+ 
+    // insert x at pos
+    arr[pos + 1] = x;
+ 
+    return arr;
+}
+void selectionSort(int list[], int length){
+  int index, smallestIndex, loc, temp;
+
+  for (index = 0; index < length-1; index++)
+    {
+      smallestIndex = index;
+
+      for ( loc = index + 1; loc < length; loc++)
+        {
+          if (list[loc] < list[smallestIndex])
+            smallestIndex = loc;
+        }
+      temp = list[smallestIndex];
+      list[smallestIndex] = list[index];
+      list[index] = temp;
+    }
+  
 }
